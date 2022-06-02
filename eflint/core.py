9ebdc2289aae7997ef1fd6975409a157f62b2304
kentaro0901 <ken0901sm@gmail.com>
2022-06-02 17:28:27 +0900

    [fix] jsonの形の修正

--- eflint/core.py
+++ eflint/core.py
@@ -16,18 +16,19 @@ def main():
     if args.mock:
         params = [
             {
-                "messages":
-                {
-                    'lineStart': 0,
-                    'columnStart': 0,
-                    'lineEnd': 0,
-                    'columnEnd': 5,
-                    'code': 'ep000',
-                    'message': 'ごみコードです',
-                    'severity': 2,
-                    'source': 'eflint',
-                    'correctable': 0
-                }
+                "messages": [
+                    {
+                        'lineStart': 0,
+                        'columnStart': 0,
+                        'lineEnd': 0,
+                        'columnEnd': 5,
+                        'code': 'ep000',
+                        'message': 'ごみコードです',
+                        'severity': 2,
+                        'source': 'eflint',
+                        'correctable': 0
+                    }
+                ]
             }
         ]
 
