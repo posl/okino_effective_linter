1c2bfceece73ab97d47e0c3709a018b9add3a42f
kentaro0901 <ken0901sm@gmail.com>
2022-06-02 16:29:21 +0900

    [add] argparseの追加

--- eflint/core.py
+++ eflint/core.py
@@ -1,19 +1,31 @@
+import argparse
 import json
 
 
-def main():
-    d = {
-        'uri': 'test.py',
-        'lineStart': 0,
-        'columnStart': 0,
-        'lineEnd': 0,
-        'columnEnd': 5,
-        'code': 'ep000',
-        'message': 'ごみコードです',
-        'severity': 2,
-        'source': 'eflint',
-        'correctable': 0
-    }
+parser = argparse.ArgumentParser(
+    prog='eflint',
+    usage='how to use',
+    description='linter for effective python.',
+)
+parser.add_argument('--mock', action='store_true', help='return test json.')
 
-    j = json.dumps(d, ensure_ascii=False)
-    print(j)
+
+def main():
+    args = parser.parse_args()
+
+    if args.mock:
+        params = {
+            'uri': 'test.py',
+            'lineStart': 0,
+            'columnStart': 0,
+            'lineEnd': 0,
+            'columnEnd': 5,
+            'code': 'ep000',
+            'message': 'ごみコードです',
+            'severity': 2,
+            'source': 'eflint',
+            'correctable': 0
+        }
+
+        j = json.dumps(params, ensure_ascii=False)
+        print(j)
