aab377ddee21e7188e71b224cd48120d9047b0fd
kentaro0901 <ken0901sm@gmail.com>
2022-06-02 17:24:36 +0900

    [update] jsonの形式を変更

--- eflint/core.py
+++ eflint/core.py
@@ -14,18 +14,22 @@ def main():
     args = parser.parse_args()
 
     if args.mock:
-        params = {
-            'uri': 'test.py',
-            'lineStart': 0,
-            'columnStart': 0,
-            'lineEnd': 0,
-            'columnEnd': 5,
-            'code': 'ep000',
-            'message': 'ごみコードです',
-            'severity': 2,
-            'source': 'eflint',
-            'correctable': 0
-        }
+        params = [
+            {
+                "messages":
+                {
+                    'lineStart': 0,
+                    'columnStart': 0,
+                    'lineEnd': 0,
+                    'columnEnd': 5,
+                    'code': 'ep000',
+                    'message': 'ごみコードです',
+                    'severity': 2,
+                    'source': 'eflint',
+                    'correctable': 0
+                }
+            }
+        ]
 
         j = json.dumps(params, ensure_ascii=False)
         print(j)
