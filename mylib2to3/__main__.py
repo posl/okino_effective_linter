01649b211bc00286b57aac8ac322a4fd9d57e727
kentaro0901 <ken0901sm@gmail.com>
2022-06-08 16:23:03 +0900

    [add] lib2to3の追加

---
+++ mylib2to3/__main__.py
@@ -0,0 +1,5 @@
+import sys
+from .main import main
+
+# python -m lib2to3で実行される
+sys.exit(main("lib2to3.fixes"))
---
+++ mylib2to3/fixes/fix_format_str.py
@@ -0,0 +1,30 @@
+"""Fixer for format string"""
+
+import re
+from .. import fixer_base
+from ..fixer_util import String
+
+
+class FixFormatStr(fixer_base.BaseFix):
+
+    BM_compatible = True
+
+    # 他パターンの追加
+    PATTERN = """
+              term< text=any '%' atom< '(' items=testlist_gexp ')' > >          
+              """
+
+    def transform(self, node, results):
+        text = results["text"]
+        items = results["items"]
+
+        new_text = text.value[1:-1]
+        quote = text.value[0]
+        args = [child.value for child in items.children if child.value != ',']
+
+        for arg in args:
+            new_text, _ = re.subn(r'%\w', f'{{{arg}}}', new_text, count=1)
+            # パディング(%10d)，リテラル（'test'），エスケープ（%%d）
+
+        new_stmt = String(f'f{quote}{new_text}{quote}', prefix=node.prefix)
+        return new_stmt
