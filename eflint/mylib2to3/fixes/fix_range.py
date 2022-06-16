af803ef83446060ce0e1c770b05b7fa81cd90f2a
kentaro0901 <ken0901sm@gmail.com>
2022-06-16 18:07:32 +0900

    [update] fix_rangeに名称を変更

---
+++ eflint/mylib2to3/fixes/fix_range.py
@@ -0,0 +1,34 @@
+"""Fixer for range to enumerate."""
+
+from .. import fixer_base
+from ..refactor import MessageContainer  # 移動したい
+
+
+class FixRange(fixer_base.BaseFix):
+
+    BM_compatible = True
+
+    PATTERN = """
+              for_stmt< 'for' any 'in' power< 'range' trailer< '(' power< 'len' trailer< '(' any ')' > > ')' > > ':' suite >
+              """
+
+    CODE = 'ef007'
+    MESSAGE = 'enumerateを使え'
+    SEVERITY = 2
+    CORRECTABLE = 0
+    DOCSURL = ''
+
+    def transform(self, node, results):
+        msg = MessageContainer(
+            node.get_lineno()-1,
+            node.get_columnno(),
+            node.get_end_lineno()-1,
+            node.get_end_columnno(),
+            self.CODE,
+            self.MESSAGE,
+            self.SEVERITY,
+            self.CORRECTABLE,
+            None
+        )
+
+        return None, msg
