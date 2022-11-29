74ab1982478c8022d15f9dd2547ab3a11155da7f
kentaro0901 <ken0901sm@gmail.com>
2022-11-29 20:42:36 +0900

    [add] 空のリストとの暗黙的な比較の変換を追加

---
+++ eflint/mylib2to3/fixes/fix_implicit_bool_not_comparison.py
@@ -0,0 +1,41 @@
+"""空のリストとの比較を暗黙的な比較に変換する
+
+* Change
+
+    if x == []:
+        print(x)
+
+into
+
+    if x:
+        print(x)
+"""
+
+
+from .. import fixer_base
+from ..msg_container import build_message
+
+
+class FixImplicitBoolNotComparison(fixer_base.BaseFix):
+
+    BM_compatible = True
+
+    PATTERN = r"""
+    comparison< id=any '==' atom< '[' ']' > >
+    """
+
+    CODE = 'C1803'
+    MESSAGE = '暗黙的な論理値への変換を使用する'
+    SEVERITY = 2
+    CORRECTABLE = 1
+    DOCSURL = ''
+
+    def transform(self, node, results):
+        new = results["id"].clone()
+        new.prefix = ''
+        msg = build_message(self, node, replacement=new)
+        return node, msg
+
+    def match(self, node):
+        r = super().match(node)
+        return r
